eb988c2ac6dd17cf44fefdd34a935acece4d60fa
thakoraarya <aarya191102@gmail.com>
2023-11-24 14:52:58 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Home} from "./components/Home";
+import {Home} from "./pages/Home";
 import {useDispatch} from "react-redux";
 import authService from "./backend/auth";
 import {login, logout} from "./store/authSlice";
